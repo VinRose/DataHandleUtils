c41ecb9e9c62b3ef42ac7558bb06e83d8a465dbc
Y Kaeko <goldencat.0303@gmail.com>
2018-06-11 21:11:24 +0900

    feat: make csv reader(data type is class)

---
+++ CsvHandle/CsvHandle/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsvHundle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var ReadData = new Data();
+        }
+    }
+
+    class Data : Person
+    {
+
+        public Data()
+        {
+            ReadCsv();
+        }
+
+        public static List<Person> p = new List<Person>();
+
+        public static void ReadCsv()
+        {
+            using (var sr = new System.IO.StreamReader(@"data.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var temp = new Person();
+                    var line = sr.ReadLine();
+                    var values = line.Split(',');
+
+                    temp.Num = values[0];
+                    temp.Name = values[1];
+                    temp.Sex = values[2];
+                    temp.Age = values[3];
+                    p.Add(temp);
+                }
+            }
+            Console.WriteLine("{0}", p[0].Name);
+            Console.WriteLine("{0}", p[1].Name);
+            Console.WriteLine("{0}", p[2].Name);
+        }
+
+    }
+
+    class Person
+    {
+        public string Num { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+
+    }
+}
